aca183044fbaa865e0ec2bcd4ed8d5acbeb60c83
chrifa amraoui <chrifaamraoui013@gmail.com>
2025-04-23 23:16:16 +0100

    Initial commit - Configuration de base

---
+++ src/main/java/org/example/handtohand/HandToHandApplication.java
@@ -0,0 +1,13 @@
+package org.example.handtohand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandToHandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandToHandApplication.class, args);
+	}
+
+}
